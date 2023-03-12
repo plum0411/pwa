d335c5cd27b856b5d0b33f7741089437ced7e6d9
plum0411 <mayble0411@gmail.com>
2023-03-13 07:22:54 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './tailwind.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      {/* <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+    </div>
+  );
+}
+
+export default App;
