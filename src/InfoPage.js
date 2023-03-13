30ad27333b3b9a85c63f7f704f7d6d13c364ca17
plum0411 <mayble0411@gmail.com>
2023-03-14 05:10:46 +0800

    add school img

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -108,10 +108,8 @@ function App() {
           <ol class="items-center sm:flex">
             <li class="relative mb-6 sm:mb-0">
               <div class="flex items-center">
-                <div class="z-10 flex items-center justify-center w-6 h-6 bg-amber-100 rounded-full ring-0 ring-white dark:bg-amber-900 sm:ring-8 dark:ring-stone-800 shrink-0">
-                  <svg aria-hidden="true" class="w-3 h-3 text-amber-800 dark:text-amber-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
-                  </svg>
+                <div class="z-10 flex items-center justify-center w-16 h-16 bg-amber-100 rounded-full ring-0 ring-white dark:bg-amber-900 sm:ring-8 dark:ring-stone-800 shrink-0">
+                  <img src='https://sites.google.com/a/mail.yaes.tc.edu.tw/tai-zhong-shi-xi-tun-qu-yong-an-guo-xiao/_/rsrc/1472691250403/xiao-hui/%E6%A0%A1%E5%BE%BD2.jpg?height=304&width=320'></img>
                 </div>
                 <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-stone-800"></div>
               </div>
