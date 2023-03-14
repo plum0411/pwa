e9c645dff57989fc44d37834042edb74b40df6f4
plum0411 <mayble0411@gmail.com>
2023-03-14 23:47:14 +0800

    info頁面字樣微調

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -115,7 +115,7 @@ function App() {
           </li>
           <li class="flex items-center text-base my-4 dark:text-white">
             <svg class="w-4 h-4 mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-            獨立思考能力
+            自我思辨
           </li>
 
           <hr class="w-full h-1 mx-auto bg-amber-200 border-0 rounded md:my-10 dark:bg-yellow-600"></hr>
