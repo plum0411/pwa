315aef41e480d1bf4d7b4ff858c8322bebeb8f19
plum_0411 <mayble0411@gmail.com>
2023-03-16 18:31:37 +0800

    修黃色警告(ALT還要修

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -96,7 +96,7 @@ function App() {
       <div className="grid sm:grid-cols-6 grid-cols-1 p-12 sm:p-24 relative overflow-hidden bg-white dark:bg-stone-900">
         <div className='col-span-1'>
           <div className='flex justify-center'>
-            <img className="rounded-full border-8 border-stone-400 mb-4 sm:w-auto w-40 justify-center" src="./img/me.png" alt="image description" />
+            <img className="rounded-full border-8 border-stone-400 mb-4 sm:w-auto w-40 justify-center" src="./img/me.png" alt="description" />
           </div>
           <hr class="w-full h-1 mx-auto bg-amber-200 border-0 rounded md:my-10 dark:bg-yellow-600"></hr>
 
@@ -124,7 +124,7 @@ function App() {
 
 
           <li class="flex text-sm my-4 dark:text-white items-center">
-            <img src='./img/phone-call.svg' class="w-4 h-4 mr-1.5 flex-shrink-0"></img>
+            <img src='./img/phone-call.svg' class="w-4 h-4 mr-1.5 flex-shrink-0" alt='phone-call'></img>
             0966-326329
           </li>
           {/* <li class="flex text-sm my-4 dark:text-white">
@@ -146,7 +146,7 @@ function App() {
           </li>
 
           <li class="flex text-sm my-4 dark:text-white items-center">
-            <img src='./img/image-solid.svg' class="w-4 h-4 mr-1.5 flex-shrink-0"></img>
+            <img src='./img/image-solid.svg' class="w-4 h-4 mr-1.5 flex-shrink-0" alt='image-solid'></img>
             {/* <img src='./img/instagram.svg' class="w-4 h-4 mr-1.5 flex-shrink-0"></img> */}
             <a href='https://photos.app.goo.gl/aCCxXUJyc1jcuykGA'>@我的黏土作品</a>
 
@@ -170,7 +170,7 @@ function App() {
           <p className="text-3xl my-4 font-black dark:text-white">Education</p>
           {/* <hr class="w-full h-0.5 mx-auto bg-stone-200 border-0 rounded md:mb-10 dark:bg-stone-800"></hr> */}
           <div className='grid sm:grid-cols-2 mt-8 justify-center items-center'>
-            <img src="./img/ia.png" className='sm:w-48 w-36 items-center'></img>
+            <img src="./img/ia.png" className='sm:w-48 w-36 items-center' alt='ia'></img>
             <p className="sm:text-lg text-base my-4 dark:text-white align-middle sm:pt-16">
               國立台中科技大學<br></br>
               <b className="sm:text-3xl text-2xl">資訊應用菁英班</b>
@@ -182,7 +182,7 @@ function App() {
             <li class="relative mb-6 sm:mb-0">
               <div class="flex items-center">
                 <div class="z-10 flex items-center justify-center w-16 h-16 bg-amber-100 rounded-full ring-0 ring-white dark:bg-amber-900 sm:ring-8 dark:ring-stone-800 shrink-0">
-                  <img src='./img/school1-1.png' class='w-full h-full'></img>
+                  <img src='./img/school1-1.png' class='w-full h-full' alt='yonann'></img>
                 </div>
                 <div class="hidden sm:flex w-full bg-stone-200 h-0.5 dark:bg-stone-800"></div>
               </div>
@@ -207,7 +207,7 @@ function App() {
                   {/* <svg aria-hidden="true" class="w-3 h-3 text-amber-800 dark:text-amber-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
                   </svg> */}
-                  <img src='./img/school2-1.png' class='w-full h-full'></img>
+                  <img src='./img/school2-1.png' class='w-full h-full' alt='fcjh'></img>
                 </div>
                 <div class="hidden sm:flex w-full bg-stone-200 h-0.5 dark:bg-stone-800"></div>
               </div>
@@ -231,7 +231,7 @@ function App() {
                   {/* <svg aria-hidden="true" class="w-3 h-3 text-amber-900 dark:text-amber-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
                   </svg> */}
-                  <img src='./img/school3-1.png' class='w-full h-full'></img>
+                  <img src='./img/school3-1.png' class='w-full h-full' alt=""></img>
                 </div>
                 <div class="hidden sm:flex w-full bg-stone-200 h-0.5 dark:bg-stone-800"></div>
               </div>
