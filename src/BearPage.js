6f5844ad8d1219146b0a2a0830262e08933b346f
plum_0411 <mayble0411@gmail.com>
2023-03-17 10:20:09 +0800

    sm logo加回來

--- src/BearPage.js
+++ src/BearPage.js
@@ -35,11 +35,11 @@ function App() {
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                   <div className="flex flex-shrink-0 items-center">
                     <a href='./'>
-                      {/* <img
+                      <img
                         className="block h-8 w-8 lg:hidden rounded-lg"
                         src='./img/bear-rect.webp'
                         alt="plum logo"
-                      /> */}
+                      />
                       <img
                         className="hidden h-8 w-8 lg:block rounded-lg"
                         src='./img/bear-rect.webp'
